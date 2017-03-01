91234420d9b2211c86ffccd047b09112cbc536aa
octavian-paraschiv <octavian.paraschiv@gmail.com>
2017-03-01 16:39:13 +0200

    Issue #244: Add support for AC3/AAC streams into the Shoutcast stram renderer. => Some proof of concept attempted no success yet

---
+++ ProTONE Suite/src/Framework/ProTONE/OPMedia.Runtime.ProTONE/Compression/WaveFile.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OPMedia.Runtime.ProTONE.Rendering.DS.BaseClasses;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace OPMedia.Runtime.ProTONE.Compression
+{
+    public static class WaveFile
+    {
+        public const uint RIFF_TAG = 0x46464952;
+        public const uint CDDA_TAG = 0x41444443;
+        public const uint WAVE_TAG = 0x45564157;
+        public const uint FMT__TAG = 0x20746D66;
+        public const uint DATA_TAG = 0x61746164;
+        public const uint WAVE_FORMAT_PCM = 0x01;
+
+        public const uint WaveHeaderSize = 38;
+        public const uint WaveFormatSize = 18;
+
+        public static byte[] ReadWaveData(string inputFile, ref WaveFormatEx wfex)
+        {
+            try
+            {
+                using (FileStream fs = new FileStream(inputFile, FileMode.Open, FileAccess.Read, FileShare.Read))
+                using (BinaryReader br = new BinaryReader(fs))
+                {
+                    br.ReadUInt32(); // RIFF_TAG
+                    uint rawSize = br.ReadUInt32(); // WaveHeaderSize + buff.Length
+                    uint buffSize = rawSize - WaveHeaderSize - sizeof(uint);
+
+                    br.ReadUInt32(); // WAVE_TAG
+                    br.ReadUInt32(); // FMT__TAG
+                    br.ReadUInt32(); // WaveFormatSize
+
+                    byte[] bytesWfex = br.ReadBytes(Marshal.SizeOf(wfex));
+
+                    var pinnedRawData = GCHandle.Alloc(bytesWfex, GCHandleType.Pinned);
+                    try
+                    {
+                        // Get the address of the data array
+                        var pinnedRawDataPtr = pinnedRawData.AddrOfPinnedObject();
+
+                        // overlay the data type on top of the raw data
+                        wfex = (WaveFormatEx)Marshal.PtrToStructure(pinnedRawDataPtr, typeof(WaveFormatEx));
+                    }
+                    finally
+                    {
+                        // must explicitly release
+                        pinnedRawData.Free();
+                    }
+
+                    br.ReadUInt32(); // DATA_TAG
+
+                    return br.ReadBytes((int)buffSize);
+                }
+            }
+            catch { }
+
+            return null;
+        }
+
+        public static void WriteWaveData(byte[] buff, WaveFormatEx wfex, string destFile)
+        {
+            using (FileStream fs = new FileStream(destFile, FileMode.Create, FileAccess.Write, FileShare.None))
+            using (BinaryWriter bw = new BinaryWriter(fs))
+            {
+                bw.Write(RIFF_TAG);
+                bw.Write((uint)(WaveHeaderSize + buff.Length));
+                bw.Write(WAVE_TAG);
+                bw.Write(FMT__TAG);
+                bw.Write(WaveFormatSize);
+
+                int size = Marshal.SizeOf(wfex);
+                byte[] arr = new byte[size];
+                IntPtr ptr = Marshal.AllocHGlobal(size);
+                Marshal.StructureToPtr(wfex, ptr, true);
+                Marshal.Copy(ptr, arr, 0, size);
+                Marshal.FreeHGlobal(ptr);
+
+                bw.Write(arr);
+                bw.Write(DATA_TAG);
+                bw.Write(buff.Length);
+                bw.Write(buff);
+            }
+        }
+    }
+}
---
+++ ProTONE Suite/src/Framework/ProTONE/OPMedia.Runtime.ProTONE/MediaProcessing/DecodeToWav.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using IO = System.IO;
+using OPMedia.Core;
+using OPMedia.Core.Utilities;
+using NAudio.Wave;
+using OPMedia.Runtime.ProTONE.Rendering.DS.BaseClasses;
+using OPMedia.Core.Logging;
+using System.Threading;
+
+namespace OPMedia.Runtime.ProTONE.MediaProcessing
+{
+    public class DecoderToWav
+    {
+        static int _calls = 0;
+        static object _decoderLock = new object();
+        string _inFile = null;
+        string _outFile = null;
+
+        public byte[] Decode(string audioType, byte[] input)
+        {
+            byte[] output = null;
+            _calls++;
+
+            lock (_decoderLock)
+            {
+                string fileType = "";
+                switch (audioType)
+                {
+                    case "audio/mpg":
+                    case "audio/mpeg":
+                        fileType = "mp3";
+                        break;
+
+                    case "audio/aac":
+                    case "audio/aacp":
+                        fileType = "aac";
+                        break;
+                }
+
+                try
+                {
+                    string _inFile = PathUtils.GetTempFilePath(string.Format("opm-dec-in-{1}.{0}", fileType,
+                        _calls % 10));
+                    string _outFile = PathUtils.GetTempFilePath(string.Format("opm-dec-out.wav"));
+
+                    DeleteFile(_outFile);
+                    DeleteFile(_inFile);
+
+                    IO.File.WriteAllBytes(_inFile, input);
+
+                    WaveFormat wf = new WaveFormat(WaveFormatEx.Cdda.nSamplesPerSec, 2);
+
+                    using (MediaFoundationReader mfr = new MediaFoundationReader(_inFile))
+                    using (ResamplerDmoStream res = new ResamplerDmoStream(mfr, wf))
+                    using (WaveFileWriter wav = new WaveFileWriter(_outFile, wf))
+                    {
+                        res.CopyTo(wav);
+
+                        wav.Close();
+                        res.Close();
+                        mfr.Close();
+                    }
+
+                    if (IO.File.Exists(_outFile))
+                    {
+                        byte[] outWav = IO.File.ReadAllBytes(_outFile);
+                        if (outWav.Length > 44)
+                        {
+                            output = new byte[outWav.Length];
+                            Array.Copy(outWav, 44, output, 0, outWav.Length - 44);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Logger.LogException(ex);
+                }
+            }
+
+            return output;
+        }
+
+        public void Dispose()
+        {
+        }
+
+        private void DeleteFile(string p)
+        {
+            try
+            {
+                if (IO.File.Exists(p))
+                {
+                    var attr = IO.File.GetAttributes(p);
+                    IO.File.SetAttributes(p, attr ^ attr);
+
+                    IO.File.Delete(p);
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.LogException(ex);
+            }
+        }
+    }
+}
