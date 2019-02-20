6f6149f0cdab74702e7ed0b0083b03f50319ab47
Octavian Paraschiv <octavian.paraschiv@gmail.com>
2019-02-20 06:54:33 -0800

    build_3.1.20

--- ProTONE Suite/src/Framework/OPMediaBase/OPMedia.Core/Version.cs
+++ ProTONE Suite/src/Framework/OPMediaBase/OPMedia.Core/Version.cs
@@ -7,7 +7,7 @@ namespace OPMedia.Core
 {
     public class SuiteVersion
     {
-        public const string Version = "3.1.19";
+        public const string Version = "3.1.20";
         public const bool IsRelease = false;
     }
 }
