de50ba9302237ceeeb332bebbe5cf9a4349edc9f
Octavian Paraschiv <octavian.paraschiv@cegeka.com>
2019-03-05 16:39:52 +0200

    more tests

--- create_buildinfo.bat
+++ create_buildinfo.bat
@@ -3,6 +3,6 @@ set CUR_YYYY=%date:~10,4%
 set CUR_MM=%date:~4,2%
 set CUR_DD=%date:~7,2%
 set DATE=%CUR_YYYY%-%CUR_MM%-%CUR_DD%
-set FILE=%1.txt
+set FILE="ProTONE Suite %APPVEYOR_BUILD_VERSION%.buildinfo.txt"
 echo %DATE% > %FILE%
 
