954fd3974a1109ec28ac65d896bda54fb22f8837
octavian-paraschiv <octavian.paraschiv@gmail.com>
2019-03-11 08:06:13 +0000

    build_3.1.62 (release:false)

--- ProTONE Suite/src/Framework/OPMediaBase/OPMedia.Core/Version.cs
+++ ProTONE Suite/src/Framework/OPMediaBase/OPMedia.Core/Version.cs
@@ -7,7 +7,7 @@ namespace OPMedia.Core
 {
     public class SuiteVersion
     {
-        public const string Version = "3.1.61";
-        public const bool IsRelease = true;
+        public const string Version = "3.1.62";
+        public const bool IsRelease = false;
     }
 }
