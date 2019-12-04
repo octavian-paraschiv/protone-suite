20bb3c108d6e70fdd48c075f0de8255c4dafed0f
octavian-paraschiv <octavian.paraschiv@gmail.com>
2019-12-04 09:20:06 +0000

    build_3.2.8 (release:true)

--- ProTONE Suite/src/Framework/OPMediaBase/OPMedia.LiteCore/Version.cs
+++ ProTONE Suite/src/Framework/OPMediaBase/OPMedia.LiteCore/Version.cs
@@ -7,7 +7,7 @@ namespace OPMedia.Core
 {
     public class SuiteVersion
     {
-        public const string Version = "3.2.7";
+        public const string Version = "3.2.8";
         public const bool IsRelease = true;
     }
 }
