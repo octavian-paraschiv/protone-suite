aed1373ded126ff523e7a19732bbb264390a0649
octavian-paraschiv <octavian.paraschiv@gmail.com>
2020-04-29 11:31:29 +0000

    build_3.3.6 (release:false)

--- ProTONE Suite/src/Framework/OPMediaBase/OPMedia.LiteCore/Version.cs
+++ ProTONE Suite/src/Framework/OPMediaBase/OPMedia.LiteCore/Version.cs
@@ -7,7 +7,7 @@ namespace OPMedia.Core
 {
     public class SuiteVersion
     {
-        public const string Version = "3.3.5";
+        public const string Version = "3.3.6";
         public const bool IsRelease = false;
     }
 }
