acb0bab94686b12151293aad904277a324e6ecda
octavian-paraschiv <octavian.paraschiv@gmail.com>
2021-08-06 07:19:27 +0000

    build_3.5.5 (release:false)

--- ProTONE Suite/src/Framework/OPMediaBase/OPMedia.LiteCore/Version.cs
+++ ProTONE Suite/src/Framework/OPMediaBase/OPMedia.LiteCore/Version.cs
@@ -7,7 +7,7 @@ namespace OPMedia.Core
 {
     public class SuiteVersion
     {
-        public const string Version = "3.5.4";
+        public const string Version = "3.5.5";
         public const bool IsRelease = false;
     }
 }
