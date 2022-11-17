594c54085323960cf109ccff525eefb114bb4e79
octavian-paraschiv <octavian.paraschiv@gmail.com>
2022-11-17 13:50:18 +0000

    build_4.0.25 (release:false)

--- ProTONE Suite/src/Framework/OPMediaBase/OPMedia.Core/Version.cs
+++ ProTONE Suite/src/Framework/OPMediaBase/OPMedia.Core/Version.cs
@@ -7,7 +7,7 @@ namespace OPMedia.Core
 {
     public class SuiteVersion
     {
-        public const string Version = "4.0.24";
+        public const string Version = "4.0.25";
         public const bool IsRelease = false;
     }
 }
